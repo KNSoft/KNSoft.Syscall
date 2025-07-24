af3ddaf48d935c9090d787c497622b650176b0be
Ratin Gao <ratin@knsoft.org>
2025-07-24 19:00:38 +0800

    1

--- Source/UnitTest/NoImport.c
+++ Source/UnitTest/NoImport.c
@@ -7,6 +7,6 @@ _Function_class_(FN_TEST_PROC)
 LOGICAL
 NoImport(VOID)
 {
-    PIMAGE_NT_HEADERS pNtHeader = Add2Ptr(&__ImageBase, __ImageBase.e_lfanew);
-    return pNtHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress == 0;
+    return ((PIMAGE_NT_HEADERS)Add2Ptr(&__ImageBase, __ImageBase.e_lfanew))->
+        OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress == 0;
 }
